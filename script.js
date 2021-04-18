d148e77979ca3e198358f6847a914841360670f1
Peter Zettl <mpzettl@gmail.com>
2021-04-18 15:52:48 +0630

    finally sort of working with one small glitch

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 // TODO: add code here
+
+window.addEventListener("load", function() {
+    let json = [];
+
+    //"./data.json"
+    /*fetch().then(function(response)*/
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+        response.json().then(function(json) {
+
+            let container = document.getElementById("container");
+            let key = 0;
+            for (json[key].id in json) {
+
+                container.insertAdjacentHTML("beforebegin",
+                    container.innerHTML = `
+                                        <div class = "astronaut">
+                                        
+                                          <div class ="bio">
+                                            <h3>${json[key].firstName} ${json[key].lastName}</h3>
+                                            <ul>
+                                              <li>Hours in space :${json[key].hoursInSpace}</li>
+                                              <li>Active: ${json[key].active}</li>
+                                              <li>Skills: ${json[key].skills}</li>
+                                              
+                                              </ul>
+                                              </div>
+                                              <img class = avatar src=${json[key].picture}></img></div>
+                                        </div>
+                                        
+                                    `);
+
+                key = (key + 1) % json.length;
+
+            }
+
+
+            //}
+
+        });
+    });
+});
+//<img class = avatar src=>Image</img>
